905aaba1e3d8d68440afa84d1ec82fffe1eaf6c8
Alind Gupta <alindgupta19@gmail.com>
2017-05-16 17:44:24 -0400

    defined fasta, fastq encoders

--- src/Data/Bioparser.hs
+++ src/Data/Bioparser.hs
@@ -8,6 +8,8 @@ module Data.Bioparser
 import Data.ByteString (ByteString)
 import Data.Attoparsec.ByteString
 import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.Monoid ((<>))
 
 import Data.Bioparser.Combinators
 import Data.Bioparser.Types
@@ -18,8 +20,16 @@ decodeFasta = parseOnly parseFasta
 decodeFastq :: ByteString -> Either String (Vector FastqRecord)
 decodeFastq = parseOnly parseFastq
 
+-- obeys the following law, should test for this more extensively
+-- decodeFasta .  encodeFasta . decodeFasta = decodeFasta
+-- encodeFasta puts a "\n" at the end of file so may not be
+-- exactly equal to input i.e.
+-- fmap (== f) (encodeFasta $ decodeFasta f) may not return True
 encodeFasta :: Vector FastaRecord -> ByteString
-encodeFasta = undefined
+encodeFasta = foldr ((<>) . fastaEncoder) ""
+  where fastaEncoder (FastaRecord (d,s)) = ">" <> d <> "\n" <> s <> "\n"
 
-encodeFastq :: Vector FastaRecord -> ByteString
-encodeFastq = undefined
+encodeFastq :: Vector FastqRecord -> ByteString
+encodeFastq = foldr ((<>) . fastqEncoder) ""
+  where fastqEncoder (FastqRecord (d,s,p,sc)) = "@" <> d <> "\n" <> s
+                                                <> "+" <> p <> sc <> "\n"
