25666f5986ea0249cd509cb4328c280111b8cc32
Alind Gupta <alindgupta@android-65c18f349d8a5ed0.gateway.2wire.net>
2017-10-05 21:58:00 -0400

    fixed indents and redundant imports

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,31 +1,23 @@
 import Criterion.Main
 
-import Control.DeepSeq
 import qualified Data.ByteString as B
 import Data.Bioparser
-import Data.Bioparser.Combinators
 import Data.Bioparser.Types
-import Data.Bioparser.Prim
-import qualified Data.Vector as V
-import Data.Attoparsec.ByteString (parse, parseOnly)
-import Control.Applicative
+import Data.Vector
 
-
-main = do
-    defaultMain [
-          bench "fasta-parser-1" $ 
-            nfIO $ fmap decodeFasta (B.readFile "test/DMproteome.fasta")
-        , bench "fasta-parser-2" $
-            nfIO $ fmap decodeFasta (B.readFile "test/HSproteome.fasta")
-        , bench "fasta-parser-1+2" $
-            nfIO $ fmap decodeFasta (B.readFile "test/Gproteome.fasta")
-        , bench "fileInput" $
-            nfIO $ B.readFile "test/Gproteome.fasta"
-        -- , bench "decEncDec" $
-            -- nfIO $ fmap decEncDec (B.readFile "test/DMproteome.fasta")
+main :: IO ()
+main = defaultMain [
+   bench "fasta-parser-1" $ 
+     nfIO $ fmap decodeFasta (B.readFile "test/DMproteome.fasta")
+ , bench "fasta-parser-2" $
+     nfIO $ fmap decodeFasta (B.readFile "test/HSproteome.fasta")
+ , bench "fasta-parser-1+2" $
+     nfIO $ fmap decodeFasta (B.readFile "test/Gproteome.fasta")
+ , bench "fileInput" $
+     nfIO $ B.readFile "test/Gproteome.fasta"
+ , bench "decEncDec" $
+       nfIO $ fmap decEncDec (B.readFile "test/DMproteome.fasta")
         ]
 
-
--- decEncDec content = (decodeFasta . encodeFasta) <$> decodeFasta content
-    
-    
+decEncDec :: B.ByteString -> Data.Vector.Vector Data.Bioparser.Types.FastaRecord
+decEncDec = decodeFasta . encodeFasta . decodeFasta
--- test/Test.hs
+++ test/Test.hs
@@ -2,30 +2,21 @@ module Main where
 
 import qualified Data.ByteString as B
 import Test.Hspec
-import Test.QuickCheck
 import Data.Bioparser
-import Data.Bioparser.Prim
-import Data.Bioparser.Combinators
-
 
 main :: IO ()
 main = hspec $ do
 
-    describe "Data.Bioparser.decodeFastq" $ do
-        it "can compose properly with encodeFastq" $ do
+    describe "Data.Bioparser.decodeFastq" $
+      it "can compose properly with encodeFastq" $ do
             fq <- B.readFile "test/SP1.fq"
             let d = decodeFastq fq
             let e = decodeFastq . encodeFastq $ d
             e `shouldBe` d
 
-    describe "Data.Bioparser.decodeFasta" $ do
+    describe "Data.Bioparser.decodeFasta" $
         it "can compose properly with encodeFasta" $ do
-            fs <- B.readFile "test/DMproteome.fasta"
+            fs <- B.readFile "test/sample.fasta"
             let d = decodeFasta fs
             let e = decodeFasta . encodeFasta $ d
             e `shouldBe` d
-
-
-            
-            
-       
