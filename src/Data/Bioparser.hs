87ef0035f89a696b9d42293392f79394ff91417c
Alind Gupta <alindgupta19@gmail.com>
2018-02-28 23:57:29 -0500

    Wrap decoders into an exposed function

--- src/Data/Bioparser.hs
+++ src/Data/Bioparser.hs
@@ -1,38 +1,25 @@
-module Data.Bioparser
-    ( decodeFasta       -- * fasta -> vector of fasta records
-    , encodeFasta       -- * vector -> fasta format
-    , decodeFastq       
-    , encodeFastq 
+odule Data.Bioparser
+    ( 
+    -- * functions
+      decodeM
+    , encodeM
     ) where
 
-{-# LANGUAGE ViewPatterns #-}
+import System.Environment
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Reader
 
-import Data.Attoparsec.ByteString
-import Data.Bioparser.Combinators
-import Data.Bioparser.Types
-import Data.ByteString (ByteString)
-import Data.Monoid ((<>))
-import Data.Vector (Vector)
-import qualified Data.ByteString.Char8 as B
-import qualified Data.Vector as V
+decodeM :: (MonadIO m) => ReaderT ParserType (Maybe (Vector (ParserOutput a)))
+decodeM p = case parseResult of
+  Done _ r -> return $ Just r
+  _        -> mzero
+  where parseResult :: Vector (ParserOutput a)
+        parseResult = decode p
 
-decodeFasta :: ByteString -> Maybe (Vector FastaRecord)
-decodeFasta x = case parseResult x of
-    Done _ r -> Just r
-    _        -> Nothing
-  where
-    parseResult = flip feed B.empty . parse parseFasta
-
-decodeFastq :: ByteString -> Vector FastqRecord
-decodeFastq x = case feed (parse parseFastq x) B.empty of
-                    Done _ r -> r
-                    _        -> error "parse error"
-
-encodeFasta :: Vector FastaRecord -> ByteString
-encodeFasta = foldr (mappend . fastaEncoder) mempty
-  where fastaEncoder (FastaRecord (d,s)) = ">" <> d <> "\n" <> s <> "\n"
-
-encodeFastq :: Vector FastqRecord -> ByteString
-encodeFastq = foldr (mappend . fastqEncoder) mempty
-  where fastqEncoder (FastqRecord (d,s,p,sc)) = "@" <> d <> "\n" <> s <> "\n"
-                                                <> "+" <> p <> "\n" <> sc <> "\n"
+encodeM Fasta = foldr (mappend . f) mempty
+  where f (FastaRecord (x,y)) = ">"
+                             <> x
+                             <> '\n'
+                             <> s
+                             <> '\n'
